26bd6a5123b643cb153f04594344e945db02ec1b
David Yue <davidyue5819@gmail.com>
2021-10-27 22:15:33 -0400

    finished ch 15

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
-int main(int argc, const char * argv[]) {
-    Chunk chunk;
-    initChunk(&chunk);
+#include "vm.h"
+int main(int argc, const char *argv[])
+{
+	initVM();
 
-    int constant = addConstant(&chunk, 1.2);
+	Chunk chunk;
+	initChunk(&chunk);
 
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
+	int constant = addConstant(&chunk, 1.2);
 
-    writeChunk(&chunk, OP_RETURN, 123);
-    disassembleChunk(&chunk, "test chunk");
-    freeChunk(&chunk);
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
 
-    return 0;
+	writeChunk(&chunk, OP_RETURN, 123);
+	disassembleChunk(&chunk, "test chunk");
+
+	freeVM();
+	freeChunk(&chunk);
+
+	return 0;
 }
