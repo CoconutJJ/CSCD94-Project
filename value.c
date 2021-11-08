40689d7f5c4243d24b098bbccd7a05ad9a247e4c
David Yue <davidyue5819@gmail.com>
2021-11-07 21:04:59 -0500

    finished ch 19

--- value.c
+++ value.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "object.h"
 #include "memory.h"
 #include "value.h"
 
@@ -40,7 +43,9 @@ void printValue(Value value)
 	case VAL_NUMBER:
 		printf("%g", AS_NUMBER(value));
 		break;
-
+	case VAL_OBJ:
+		printObject(value);
+		break;
 	default:
 		break;
 	}
@@ -59,7 +64,11 @@ bool valuesEqual(Value a, Value b) {
         return true;
     case VAL_NUMBER:
         return AS_NUMBER(a) == AS_NUMBER(b);
-    default:
+    case VAL_OBJ:
+		ObjString * aString = AS_STRING(a);
+		ObjString * bString = AS_STRING(b);
+		return aString->length == bString->length && memcmp(aString->chars, bString->chars, aString->length) == 0;
+	default:
         return false;
     }
 
