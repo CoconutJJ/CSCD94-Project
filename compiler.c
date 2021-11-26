59d4c35060fbc726c9b4934cb0b3e03b97a7dce1
David Yue <davidyue5819@gmail.com>
2021-11-25 19:50:29 -0500

    finish ch 25

--- compiler.c
+++ compiler.c
@@ -38,7 +38,13 @@ typedef struct {
 typedef struct {
 	Token name;
 	int depth;
+	bool isCaptured;
 } Local;
+
+typedef struct {
+	uint8_t index;
+	bool isLocal;
+} Upvalue;
 
 typedef enum { TYPE_FUNCTION, TYPE_SCRIPT } FunctionType;
 
@@ -48,6 +54,7 @@ typedef struct Compiler {
 	FunctionType type;
 	Local locals[UINT8_COUNT];
 	int localCount;
+	Upvalue upvalues[UINT8_COUNT];
 	int scopeDepth;
 } Compiler;
 
@@ -198,11 +205,13 @@ static void initCompiler(Compiler *compiler, FunctionType type)
 	current = compiler;
 
 	if (type != TYPE_SCRIPT) {
-		current->function->name = copyString(parser.previous.start, parser.previous.length);
+		current->function->name = copyString(parser.previous.start,
+						     parser.previous.length);
 	}
 
 	Local *local = &current->locals[current->localCount++];
 	local->depth = 0;
+	local->isCaptured = false;
 	local->name.start = "";
 	local->name.length = 0;
 }
@@ -214,7 +223,7 @@ static ObjFunction *endCompiler()
 #ifdef DEBUG_PRINT_CODE
 	if (!parser.hadError) {
 		disassembleChunk(currentChunk(), function->name != NULL ?
-							       function->name->chars :
+							 function->name->chars :
 							       "<script>");
 	}
 #endif
@@ -234,7 +243,12 @@ static void endScope()
 	while (current->localCount > 0 &&
 	       current->locals[current->localCount - 1].depth >
 		       current->scopeDepth) {
-		emitByte(OP_POP);
+		if (current->locals[current->localCount - 1].isCaptured) {
+			emitByte(OP_CLOSE_UPVALUE);
+		} else {
+			emitByte(OP_POP);
+		}
+
 		current->localCount--;
 	}
 }
@@ -269,6 +283,47 @@ static int resolveLocal(Compiler *compiler, Token *name)
 	return -1;
 }
 
+static int addUpvalue(Compiler *compiler, uint8_t index, bool isLocal)
+{
+	int upvalueCount = compiler->function->upvalueCount;
+
+	for (int i = 0; i < upvalueCount; i++) {
+		Upvalue *upvalue = &compiler->upvalues[i];
+		if (upvalue->index == index && upvalue->isLocal == isLocal) {
+			return i;
+		}
+	}
+
+	if (upvalueCount == UINT8_COUNT) {
+		error("Too many closure variables in function.");
+		return 0;
+	}
+
+	compiler->upvalues[upvalueCount].isLocal = isLocal;
+	compiler->upvalues[upvalueCount].index = index;
+	return compiler->function->upvalueCount++;
+}
+
+static int resolveUpvalue(Compiler *compiler, Token *name)
+{
+	if (compiler->enclosing == NULL)
+		return -1;
+
+	int local = resolveLocal(compiler->enclosing, name);
+
+	if (local != -1) {
+		compiler->enclosing->locals[local].isCaptured = true;
+		return addUpvalue(compiler, (uint8_t)local, true);
+	}
+
+	int upvalue = resolveUpvalue(compiler->enclosing, name);
+	if (upvalue != -1) {
+		return addUpvalue(compiler, (uint8_t)upvalue, false);
+	}
+
+	return -1;
+}
+
 static void addLocal(Token name)
 {
 	if (current->localCount == UINT8_COUNT) {
@@ -279,6 +334,7 @@ static void addLocal(Token name)
 	Local *local = &current->locals[current->localCount++];
 	local->name = name;
 	local->depth = -1;
+	local->isCaptured = false;
 }
 
 static void declareVariable()
@@ -330,7 +386,8 @@ static void defineVariable(uint8_t global)
 	emitBytes(OP_DEFINE_GLOBAL, global);
 }
 
-static uint8_t argumentList() {
+static uint8_t argumentList()
+{
 	uint8_t argCount = 0;
 	if (!check(TOKEN_RIGHT_PAREN)) {
 		do {
@@ -339,7 +396,7 @@ static uint8_t argumentList() {
 				error("Can't have more than 255 arguments.");
 			}
 			argCount++;
-		} while(match(TOKEN_COMMA));
+		} while (match(TOKEN_COMMA));
 	}
 	consume(TOKEN_RIGHT_PAREN, "Expect ')' after arguments.");
 	return argCount;
@@ -396,7 +453,8 @@ static void binary(bool canAssign)
 	}
 }
 
-static void call(bool canAssign) {
+static void call(bool canAssign)
+{
 	uint8_t argCount = argumentList();
 	emitBytes(OP_CALL, argCount);
 }
@@ -457,6 +515,9 @@ static void namedVariable(Token name, bool canAssign)
 	if (arg != -1) {
 		getOp = OP_GET_LOCAL;
 		setOp = OP_SET_LOCAL;
+	} else if ((arg = resolveUpvalue(current, &name)) != -1) {
+		getOp = OP_GET_UPVALUE;
+		setOp = OP_SET_UPVALUE;
 	} else {
 		arg = identifierConstant(&name);
 		getOp = OP_GET_GLOBAL;
@@ -601,7 +662,13 @@ static void function(FunctionType type)
 	block();
 
 	ObjFunction *function = endCompiler();
-	emitBytes(OP_CONSTANT, makeConstant(OBJ_VAL(function)));
+
+	emitBytes(OP_CLOSURE, makeConstant(OBJ_VAL(function)));
+
+	for (int i = 0; i < function->upvalueCount; i++) {
+		emitByte(compiler.upvalues[i].isLocal ? 1 : 0);
+		emitByte(compiler.upvalues[i].index);
+	}
 }
 
 static void funDeclaration()
@@ -710,8 +777,8 @@ static void printStatement()
 	emitByte(OP_PRINT);
 }
 
-static void returnStatement() {
-
+static void returnStatement()
+{
 	if (current->type == TYPE_SCRIPT) {
 		error("Can return from top-level code.");
 	}
--- debug.c
+++ debug.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "debug.h"
+#include "object.h"
 #include "value.h"
 
 void disassembleChunk(Chunk *chunk, const char *name)
@@ -25,13 +26,15 @@ static int simpleInstruction(const char *name, int offset)
 	return offset + 1;
 }
 
-static int byteInstruction(const char * name, Chunk * chunk, int offset) {
+static int byteInstruction(const char *name, Chunk *chunk, int offset)
+{
 	uint8_t slot = chunk->code[offset + 1];
 	printf("%-16s %4d\n", name, slot);
 	return offset + 2;
 }
 
-static int jumpInstruction(const char * name, int sign, Chunk * chunk, int offset) {
+static int jumpInstruction(const char *name, int sign, Chunk *chunk, int offset)
+{
 	uint16_t jump = (uint16_t)(chunk->code[offset + 1] << 8);
 	jump |= chunk->code[offset + 2];
 	printf("%-16s %4d -> %d\n", name, offset, offset + 3 + sign * jump);
@@ -70,6 +73,10 @@ int disassembleInstruction(Chunk *chunk, int offset)
 		return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
 	case OP_SET_GLOBAL:
 		return constantInstruction("OP_SET_GLOBAL", chunk, offset);
+	case OP_GET_UPVALUE:
+		return byteInstruction("OP_GET_UPVALUE", chunk, offset);
+	case OP_SET_UPVALUE:
+		return byteInstruction("OP_SET_UPVALUE", chunk, offset);
 	case OP_EQUAL:
 		return simpleInstruction("OP_EQUAL", offset);
 	case OP_GREATER:
@@ -96,6 +103,27 @@ int disassembleInstruction(Chunk *chunk, int offset)
 		return jumpInstruction("OP_JUMP_IF_FALSE", 1, chunk, offset);
 	case OP_LOOP:
 		return jumpInstruction("OP_LOOP", -1, chunk, offset);
+	case OP_CALL:
+		return byteInstruction("OP_CALL", chunk, offset);
+	case OP_CLOSURE: {
+		offset++;
+		uint8_t constant = chunk->code[offset++];
+		printf("%-16s %4d ", "OP_CLOSURE", constant);
+		printValue(chunk->constants.values[constant]);
+		printf("\n");
+		ObjFunction *function =
+			AS_FUNCTION(chunk->constants.values[constant]);
+		for (int j = 0; j < function->upvalueCount; j++) {
+			int isLocal = chunk->code[offset++];
+			int index = chunk->code[offset++];
+			printf("%04d      |                     %s %d\n",
+			       offset - 2, isLocal ? "local" : "upvalue",
+			       index);
+		}
+		return offset;
+	}
+	case OP_CLOSE_UPVALUE:
+		return simpleInstruction("OP_CLOSE_UPVALUE", offset);
 	case OP_RETURN:
 		return simpleInstruction("OP_RETURN", offset);
 	default:
